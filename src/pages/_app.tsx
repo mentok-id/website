2a101f06ee567f6a349730722fa8738e9de5634b
Barnando Akbarto Hidayatullah <51733515+xxidbr9@users.noreply.github.com>
2021-05-11 09:54:43 +0700

    feat(base): add no script GTM (#5)
    * feat(base): structurize project
    
    * feat(base): done implementing atomic pattern
    
    * feat(base): adding author
    
    * add google tag manager
    
    * feat(base): add no script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import Meta from "@app/components/atoms/Meta"
+import NoScript from "@app/components/atoms/NoScript"
+import React from "react"
 import "../assets/scss/global.scss"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Meta />
+      <Component {...pageProps} />
+      <NoScript />
+    </>
+  )
 }
 
 export default MyApp
